903a61ba31008b2ea7d5bbcb5c293725912894ee
Manlove <35545813+Manlove@users.noreply.github.com>
2018-01-17 16:38:25 -0600

    Add files via upload

---
+++ Black_Jack.py
@@ -0,0 +1,18 @@
+class Blackjack():
+    pass
+
+
+
+class Dealer():
+    def __init__():
+        self.shuffleDeck()
+    
+    def shuffleDeck():
+        self.deck = [2,3,4,5,6,7,8,9,10,"J","K","Q","A",2,3,4,5,6,7,8,9,10,"J","K","Q","A",2,3,4,5,6,7,8,9,10,"J","K","Q","A",2,3,4,5,6,7,8,9,10,"J","K","Q","A"]
+
+    def deal():
+        pass
+    
+class Player():
+    def __init__():
+        pass
